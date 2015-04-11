969f46203d0c7c1484b8abec966dc520faffb8a8
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-04-11 16:04:55 -0400

    commented out js error, updated gitignore

--- static/js/planets.js
+++ static/js/planets.js
@@ -1,5 +1,12 @@
 "use strict";
+var planets_data;
 
-var planets_data = $.
+// $.get("planetsdata", function (data) {
+//     planets_data = data;
+// });
 
+
+// d3.json("planetsdata", function (data) {
+//     console.log(data);
+// });
 
